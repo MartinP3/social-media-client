f3a071d56cca2ced0381dc2dc38a932d13d2f9b9
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-11-13 12:49:38 +0100

    new create post timings

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -18,32 +18,31 @@ describe("it creates a post after logging in", () => {
       cy.wait(500);
       cy.get("input[type='password']:visible").type("password");
       cy.get("button[type='submit']:visible").click();
-      cy.wait(2000);
+      cy.wait(3000);
       cy.then(() => expect(localStorage.getItem("token")).to.not.be.null);
       cy.then(() => expect(localStorage.getItem("profile")).to.not.be.null);
-      cy.wait(2000);
+      cy.wait(3000);
     });
     // then clicks "new post"
     cy.get("a[href='./?view=post']").click();
-    cy.wait(500);
+    cy.wait(1500);
 
     // Checks that the url is correct
     // cy.url().should("include", "post");
     // Fills in post data
     cy.get("#postTitle").should("exist").type("the title");
     cy.get("#postTags").should("exist").type("cypress");
-
     // Post media tends to cover the screen so much that it can't even see submit button
     // I tried adding scrollTo actions but no dice with my attempts.
 
     // cy.get("#postMedia").should("exist").type("https://picsum.photos/200/200");
+
     cy.get("#postBody").should("exist").type("bottom text");
-
-    // I have tried to keep this in seperate "it(s)" but it keeps taking
-    //away the token and profile so albeit ugly I will keep it this way for now
-
     cy.get("button[data-action='submit']").click();
     cy.wait(4000);
     cy.get("button[data-action='delete']:visible").click();
   });
 });
+
+// I have tried to keep this in seperate "it(s)" but it keeps taking
+//away the token and profile so albeit ugly I will keep it this way for now
