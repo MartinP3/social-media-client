6eef35efcb447a6eba1937c9a468ea238ce8320c
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-11-13 09:53:27 +0100

    new test files for cypress testing

--- cypress/e2e/create.cy.js
+++ cypress/e2e/create.cy.js
@@ -2,11 +2,12 @@ describe("it creates a post after logging in", () => {
   it("opens up the site", () => {
     cy.visit("http://127.0.0.1:8080/");
     cy.clearLocalStorage();
+    cy.wait(500);
     cy.get("#registerForm").within(() => {
       cy.get(".btn-close:visible").click();
       cy.wait(500);
     });
-    cy.get("button[data-auth='login']:visible").click({ force: true });
+    cy.get("button[data-auth='login']:visible").click();
   });
 
   // Logs in with a registered account's details, posts and deletes
@@ -17,7 +18,7 @@ describe("it creates a post after logging in", () => {
         .should("exist")
         .type(`thefool@noroff.no`);
       cy.get("input[type='password']:visible").should("exist").type("password");
-      cy.get("button[type='submit']:visible").click({ force: true });
+      cy.get("button[type='submit']:visible").click();
       cy.wait(2000);
     });
     // then clicks "new post"
@@ -29,8 +30,8 @@ describe("it creates a post after logging in", () => {
     // Fills in post data
     cy.get("#postTitle").should("exist").type("the title");
     cy.get("#postTags").should("exist").type("cypress");
+    cy.get("#postMedia").should("exist").type("https://picsum.photos/200/200");
     cy.get("#postBody").should("exist").type("bottom text");
-    // cy.get("#postMedia").should("exist").type("https://picsum.photos/200/200");
 
     // I have tried to keep this in seperate "it(s)" but it keeps taking
     //away the token and profile so albeit ugly I will keep it this way for now
