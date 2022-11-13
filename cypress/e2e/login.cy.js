885a2d9f5fba19d4a199c63bebc9fb4d9378e5b2
Martin P <70173574+MartinP3@users.noreply.github.com>
2022-11-13 13:37:07 +0100

    should avoid being stuck on home page

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,18 +1,36 @@
 describe("Login authentication", () => {
   beforeEach(() => {
     cy.clearLocalStorage();
+  });
+  // Tries to log in with bad account details
+  it("CAN'T login with bad credentials", () => {
     cy.visit("/");
-    cy.wait(1000);
+    cy.wait(2000);
     cy.get("#registerForm").within(() => {
       cy.get(".btn-close:visible").click();
       cy.wait(500);
     });
     cy.get("button[data-auth='login']:visible").click();
     cy.wait(2000);
+    cy.get("#loginForm").within(() => {
+      cy.wait(500);
+      cy.get("input[type='email']:visible")
+        .should("exist")
+        .type(`bad@email.com`);
+      cy.get("input[type='password']:visible").should("exist").type("wrngPwd");
+      cy.get("button[type='submit']:visible").click();
+    });
   });
-
   // Logs in with a registered account's details
   it("CAN login with good credentials", () => {
+    cy.visit("/");
+    cy.wait(2000);
+    cy.get("#registerForm").within(() => {
+      cy.get(".btn-close:visible").click();
+      cy.wait(500);
+    });
+    cy.get("button[data-auth='login']:visible").click();
+    cy.wait(2000);
     cy.get("#loginForm").within(() => {
       cy.wait(500);
       cy.get("input[type='email']:visible")
@@ -22,18 +40,6 @@ describe("Login authentication", () => {
       cy.get("input[type='password']:visible").should("exist").type("password");
       cy.get("button[type='submit']:visible").click();
       cy.wait(1000);
-    });
-  });
-
-  // Tries to log in with bad account details
-  it("CAN'T login with bad credentials", () => {
-    cy.get("#loginForm").within(() => {
-      cy.wait(500);
-      cy.get("input[type='email']:visible")
-        .should("exist")
-        .type(`bad@email.com`);
-      cy.get("input[type='password']:visible").should("exist").type("wrngPwd");
-      cy.get("button[type='submit']:visible").click();
     });
   });
 });
